09e274bc921481c03ae0bc507c17441518e61564
ABUZAR-2003 <abuzaryaseen118@gmail.com>
2025-07-08 13:09:00 +0530

    Update day41_second_largest.py

--- submissions/Abuzar_Yaseen/day41_second_largest.py
+++ submissions/Abuzar_Yaseen/day41_second_largest.py
@@ -1,9 +1,11 @@
 num = input("Enter numbers: ")
-input_line = list(map(int,num.strip().split()))
+input_line = list(map(int, num.strip().split()))
 
-unique = list(set(input_line))
+unique_numbers = list(set(input_line))
 
-unique.sort(reverse=True)
-second_largest = unique[1]
-print("Second Largest Number: ",second_largest)
-
+unique_numbers.sort(reverse=True)
+if len(unique_numbers) >= 2:
+    second_largest = unique_numbers[1]
+    print("Second Largest Number:", second_largest)
+else:
+    print("Not enough unique numbers to find the second largest.")
